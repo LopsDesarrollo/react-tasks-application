92b8f623904b6ebadc05fc95f3a6737afa125271
Alex Villagran <alex.villagran.mancilla@gmail.com>
2024-12-11 10:00:47 -0600

    Se integro diseño con el framework tailwindcss

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -18,19 +18,26 @@ function TaskForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="max-w-md mx-auto">
+      <h1 className="text-2xl font-bold text-white mb-3">Crear tarea</h1>
+      <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4 rounded-md">
       <input
         placeholder="Escribe tu tarea"
         onChange={(e) => setTitle(e.target.value)}
         value={title}
+        className="bg-slate-300 w-full mb-2"
         autoFocus
       />
       <textarea placeholder="Escribe la descripcion de la tarea"
         onChange={(e)=> setDescription(e.target.value)}
+        className="bg-slate-300 w-full mb-2"
         value={description}
         ></textarea>
-      <button>Guardar</button>
+      <button
+        className="bg-indigo-800 px-3 py-1 text-white rounded-md hover:bg-indigo-400"
+      >Guardar</button>
     </form>
+    </div>
   );
 }
 
--- src/data/tasks.js
+++ src/data/tasks.js
@@ -2,16 +2,21 @@ export const tasks = [
   {
     id: 0,
     title: "Mi primer tarea",
-    description: "mi primer tarea del mes",
+    description: "Mi primer tarea del mes",
   },
   {
     id: 1,
     title: "Mi segunda tarea",
-    description: "mi segunda tarea del mes",
+    description: "Mi segunda tarea del mes",
   },
   {
     id: 2,
     title: "Mi tercer tarea",
-    description: "mi tercer tarea del mes",
+    description: "Mi tercer tarea del mes",
   },
+  {
+    id: 3,
+    title: "Mi cuarta tarea",
+    description: "Mi cuarta tarea del mes",
+  }
 ];
